e11578e6598ee003bef2cdafa9941fe8ca6589fe
dhavamani16 <dhavamani17092005@gmail.com>
2025-11-26 18:04:32 +0530

    Add userRoutes to handle user registration, login, and related routes

--- task-manager/client/routes/taskRoutes.js
+++ task-manager/client/routes/taskRoutes.js
@@ -1,31 +1,10 @@
-import express from "express";
-import Task from "../models/Task.js";
-
+import express from "express"
+import { addTask, getTask, removeTask} from "../controllers/taskController.js"
+import requireAuth from "../middleware/requireAuth.js";
 const router = express.Router();
 
-// Create task
-router.post("/task", async (req, res) => {
-  const task = new Task(req.body);
-  await task.save();
-  res.json(task);
-});
-
-// Get all tasks
-router.get("/task", async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
-
-// Update task
-router.put("/task/:id", async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(task);
-});
-
-// Delete task
-router.delete("/task/:id", async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.json({ message: "Task deleted" });
-});
+router.post("/addTask", requireAuth, addTask)
+router.get("/getTask",requireAuth, getTask)
+router.get("/removeTask",requireAuth, removeTask)
 
 export default router;
